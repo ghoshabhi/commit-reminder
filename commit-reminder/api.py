822442463271e91626f1d14e1a937de7d44f184f
Abhishek Ghosh <abhighosh18@gmail.com>
2017-02-15 22:39:59 -0800

    Feat: Setup Github OAuth

--- commit-reminder/api.py
+++ commit-reminder/api.py
@@ -1,51 +1,65 @@
 #!/usr/bin/env python
 import requests
-from flask import Flask, request, jsonify, session, redirect, \
+from flask import Flask, request, jsonify, redirect, \
                  render_template, send_from_directory
+from flask import session as login_session
+from flask import make_response
+import json
+import requests
+import random
+import string
+
 
 from flask_api import FlaskAPI
 
 app = Flask(__name__)
-# app.config['GITHUB_CLIENT_ID'] = ''
-# app.config['GITHUB_CLIENT_SECRET'] = ''
 
 @app.route('/')
 def showLogin():
-    return render_template("login.html")
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', state=state)
 
-# Here we're using the /callback route.
+# Using the /callback route to handle authentication.
 @app.route('/callback')
 def callback_handling():
+    print "Hi AJAX!"
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter!'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
     code = request.args.get('code')
-    print "code: {code}".format(code=code)
-    json_header = {'content-type': 'application/json'}
+    print "code: {code}".format(code=json.dumps(code))
 
-    token_url = "https://{domain}/oauth/token".format(domain='ghoshabhi.auth0.com')
-
-    token_payload = {
-        'client_id':     'Loz6vT3OWA0xt6smDzJ8oVprZwcwIwGA',
-        'client_secret': '4DhQhrGHsN7QtIi2U-VKVqO6iWj_Z1-2HDYuUtrHXswBPYHDEpDblY6vPM0mmbQk',
-        'redirect_uri':  'https://localhost:5000/callback',
-        'code':          code,
-        'grant_type':    'authorization_code'
-    }
-
-    token_info = requests.post(token_url,
-                              data=json.dumps(token_payload),
-                              headers = json_header) \
-                             .json()
-
-    user_url = "https://{domain}/userinfo?access_token={access_token}" \
-      .format(domain='ghoshabhi.auth0.com', access_token=token_info['access_token'])
-
-    user_info = requests.get(user_url).json()
-
-    # We're saving all user information into the session
-    session['profile'] = user_info
+    #json_header = {'content-type': 'application/json'}
+    #
+    # token_url = "https://{domain}/oauth/token".format(domain='ghoshabhi.auth0.com')
+    #
+    # token_payload = {
+    #     'client_id':     'Loz6vT3OWA0xt6smDzJ8oVprZwcwIwGA',
+    #     'client_secret': '4DhQhrGHsN7QtIi2U-VKVqO6iWj_Z1-2HDYuUtrHXswBPYHDEpDblY6vPM0mmbQk',
+    #     'redirect_uri':  'https://localhost:5000/callback',
+    #     'code':          code,
+    #     'grant_type':    'authorization_code'
+    # }
+    #
+    # token_info = requests.post(token_url,
+    #                           data=json.dumps(token_payload),
+    #                           headers = json_header) \
+    #                          .json()
+    #
+    # user_url = "https://{domain}/userinfo?access_token={access_token}" \
+    #   .format(domain='ghoshabhi.auth0.com', access_token=token_info['access_token'])
+    #
+    # user_info = requests.get(user_url).json()
+    #
+    # # We're saving all user information into the session
+    # session['profile'] = user_info
 
     # Redirect to the User logged in page that you want here
     # In our case it's /dashboard
-    return redirect('/hello')
 
 @app.route('/hello')
 def sayHello():
