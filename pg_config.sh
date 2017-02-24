32ce4738193e946a3ee582e73f47bdef3acf9bb0
Abhishek Ghosh <abhighosh18@gmail.com>
2017-02-24 15:29:19 -0800

    Init: Init vagrant setup

--- pg_config.sh
+++ pg_config.sh
@@ -5,15 +5,16 @@ apt-get -qqy install python-pip
 pip install bleach
 pip install oauth2client
 pip install requests
+pip install flask-cors
 pip install httplib2
 pip install redis
 pip install passlib
 pip install itsdangerous
 pip install flask-httpauth
 su postgres -c 'createuser -dRS vagrant'
-su vagrant -c 'createdb'
-su vagrant -c 'createdb forum'
-su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
+#su vagrant -c 'createdb'
+#su vagrant -c 'createdb forum'
+#su vagrant -c 'psql forum -f /vagrant/forum/forum.sql'
 
 vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
 echo -e $vagrantTip > /etc/motd
