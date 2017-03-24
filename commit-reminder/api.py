db7a0b264c86ce1bd8dfe5529b8d35aa03c7d1fc
Abhishek Ghosh <abhighosh18@gmail.com>
2017-03-24 09:27:41 -0700

    Update

--- commit-reminder/api.py
+++ commit-reminder/api.py
@@ -191,4 +191,8 @@ def getCommits(username, repo_name):
 if __name__ == "__main__":
     app.secret_key = "fart_fart"
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(
+        host='0.0.0.0',
+        port=port
+    )
